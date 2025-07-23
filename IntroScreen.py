4d3e091dd1309bcd32571f137dc37bdb45f40321
Leonid Elkin <lnd.elkn@gmail.com>
2025-07-23 14:07:17 +0300

    Added finished and cleared Introsequence

---
+++ IntroScreen.py
@@ -0,0 +1,222 @@
+from PyQt5.QtWidgets import QWidget
+from PyQt5.QtGui import QPainter, QFont, QColor
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
+from UI.StarLogoWidget import StarLogoWidget
+
+
+class IntroScreen(QWidget):
+    
+    #File signals
+    IntroStarted = pyqtSignal()
+    FadeInFinished = pyqtSignal()
+    FadeOutStarted = pyqtSignal()
+    IntroFinished = pyqtSignal()
+
+    def __init__(self, displayDuration=2000, fadeOutDuration=1000):
+        print("DEBUG: INTRO SEQUENCE BEGUN")
+        super().__init__()
+
+        # Create the StarLogo widget as a child of this intro screen
+        self.StarLogo = StarLogoWidget(self)
+
+        # Hardcoded to start fully visible
+        self.opacity = 1.0
+        self.StarLogo.setOpacity(self.opacity)
+
+
+
+        self.showFullScreen()
+
+        #stupid stylesheet 
+        self.setStyleSheet("background-color: black;")
+
+        self.DisplayDuration = displayDuration
+        self.FadeOutDuration = fadeOutDuration
+
+        #No fade in so just call immediately - Not bothered to fix properly
+        self.IntroStarted.emit()
+        self.FadeInFinished.emit()  
+
+        # Setup fade-out timer with original logic
+        self.fadeOutTimer = QTimer(self)
+        self.fadeOutTimer.timeout.connect(self.fadeOutStep)
+        self.fadeOutStepAmount = 30 / self.FadeOutDuration
+
+        #Schedule fade-out to start after the display duration)
+        QTimer.singleShot(self.DisplayDuration, self.StartFadeOut)
+
+    def fadeOutStep(self):
+        # Decrease opacity
+        self.opacity -= self.fadeOutStepAmount
+
+        if self.opacity <= 0.0:
+            
+            #Stop fading and emmit signal that intro stopped
+            self.opacity = 0.0
+            self.fadeOutTimer.stop()
+            print("DEBUG: INTRO FINISHED")
+            self.IntroFinished.emit()
+
+        #Star logo opacity change
+        self.StarLogo.setOpacity(self.opacity)
+        #Force a paint event
+        self.update()
+
+    def StartFadeOut(self):
+        print("DEBUG: INTRO FADE OUT STARTED")
+        #Opacity decreasing step calling every 30ms
+        self.FadeOutStarted.emit()
+        self.fadeOutTimer.start(30)
+
+
+    def paintEvent(self, widgetEvent):
+        # This is a special QWidget method that is called automatically when the widget is resized or first created
+
+        painter = QPainter(self)  
+        painter.setRenderHint(QPainter.Antialiasing)  #I feel so smart  (Copied of google)
+
+        #Background color
+        painter.fillRect(self.rect(), Qt.black)
+
+        #Set painter opacity
+        painter.setOpacity(self.opacity)
+
+        # Get widget dimensions
+        SCREEN_WIDTH = self.width()
+        SCREEN_HEIGHT = self.height()
+        CentreY = SCREEN_HEIGHT / 2  
+
+
+
+        #Star Logo creation
+
+        starSize = int(min(SCREEN_WIDTH, SCREEN_HEIGHT) * 0.5)
+        self.StarLogo.resize(starSize, starSize)
+
+
+        starX = (SCREEN_WIDTH - starSize) / 2
+        starY = (SCREEN_HEIGHT / 1.7) - starSize * 0.8
+
+        self.StarLogo.move(int(starX), int(starY))
+
+
+
+        # =-= Layout Vars =-=
+
+        #Margin off left edge
+        leftMargin = int(SCREEN_WIDTH * -0.1)  
+        subtitleLeftMargin = int(SCREEN_HEIGHT * 0.15)
+
+
+        #Spacing between titles
+        titleSpacing = int(SCREEN_HEIGHT * 0.075) 
+        titleOffsetY = int(min(SCREEN_WIDTH, SCREEN_HEIGHT) * 0.05)   # Vertical offset from center for titles
+
+        smallTextOffsetY = int((titleOffsetY + 3 * titleSpacing) * 1.05)  #Spacing below titles
+        subtitleOffsetY = smallTextOffsetY + int(SCREEN_HEIGHT * 0.1)   #Y offset of subtitle
+
+
+        shadowColor = QColor(0, 140, 255, 140)  #Text shadow color
+
+        # Fonts sized relative to widget height for responsiveness
+        fontTitle = QFont("Times New Roman", int(SCREEN_HEIGHT * 0.06), QFont.Bold)
+        fontSmall = QFont("Times New Roman", int(SCREEN_HEIGHT * 0.03), QFont.Bold)
+        fontSubtitle = QFont("Times New Roman", int(SCREEN_HEIGHT * 0.035), QFont.Bold)
+
+        # Titles and text
+        titleRow1 = "SUSAN"
+        titleRow2 = "YAVALATH"
+        titleRow3 = "PENTALATH"
+
+        smallText = "BY LEONID ELKIN"
+        subtitleText = "Abstract hexagonal strategy games"
+
+
+
+
+
+
+        painter.translate(SCREEN_WIDTH / 2, 0)
+        painter.scale(0.79, 1) #Make font fitting
+        painter.translate(-SCREEN_WIDTH / 2, 0)
+
+        # =-=TITLES=-=
+
+        painter.setFont(fontTitle)
+
+        offsetListForTitles = [(1,1),(2,2),(3,3),(4,4)]
+        offsetListForSubtitles = [(1,1), (2,2), (3,3)]
+
+        title1Offset = int(CentreY) + titleOffsetY + 0 * titleSpacing
+        title2Offset = int(CentreY) + titleOffsetY + 1 * titleSpacing
+        title3Offset = int(CentreY) + titleOffsetY + 2 * titleSpacing
+
+
+        for offsetX, offsetY in offsetListForTitles:
+
+            #Title 1 shadow
+            painter.setPen(shadowColor)
+            painter.drawText(leftMargin + offsetX, title1Offset + offsetY, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow1)
+
+            #Title 2 shadow
+            painter.setPen(shadowColor)
+            painter.drawText(leftMargin + offsetX, title2Offset + offsetY, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow2)
+
+            #Title 3 shadow
+            painter.setPen(shadowColor)
+            painter.drawText(leftMargin + offsetX, title3Offset + offsetY, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow3)
+
+        #Title1 body
+        painter.setPen(Qt.white)
+        painter.drawText(leftMargin, title1Offset, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow1)
+
+        #Title2 body
+        painter.setPen(Qt.white)
+        painter.drawText(leftMargin, title2Offset, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow2)
+
+        #Title3 body
+        painter.setPen(Qt.white)
+        painter.drawText(leftMargin, title3Offset, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, titleRow3)
+
+
+
+
+        #SUBTITLE
+        painter.setFont(fontSmall)
+
+        yOffsetSubtitle = int(CentreY) + smallTextOffsetY
+
+        for OffsetX, OffsetY in offsetListForSubtitles:
+
+            painter.setPen(shadowColor)
+            painter.drawText(leftMargin + OffsetX, yOffsetSubtitle + OffsetY, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, smallText)
+
+        painter.setPen(Qt.white)
+        painter.drawText(leftMargin, yOffsetSubtitle, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.1), Qt.AlignLeft, smallText)
+
+
+
+        #FOOTNOTE
+        painter.setFont(fontSubtitle)
+
+        yOffsetFootnote = int(CentreY) + subtitleOffsetY
+
+        for OffsetX, OffsetY in offsetListForSubtitles:
+
+            painter.setPen(shadowColor)
+            painter.drawText(subtitleLeftMargin + OffsetX, yOffsetFootnote + OffsetY, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.08), Qt.AlignLeft, subtitleText)
+
+        painter.setPen(Qt.white)
+        painter.drawText(subtitleLeftMargin, yOffsetFootnote, SCREEN_WIDTH, int(SCREEN_HEIGHT * 0.08), Qt.AlignLeft, subtitleText)
+
+
+
+
+
+
+    def keyPressEvent(self, event):
+        #intro skipper
+        if event.key() == Qt.Key_Escape:
+            #Call Introfinished early   
+            print("DEBUG: INTRO FINISHED (skipped)")
+            self.IntroFinished.emit()
